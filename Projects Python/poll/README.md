f51092bda8eda3a3113a2ddfe37f30ec94b271ec
Anton Robert <74978336+UnX-Robert@users.noreply.github.com>
2021-08-13 14:53:23 +0300

    Delete Projects Python/poll directory

--- Projects Python/poll/README.md
+++
@@ -1,6 +0,0 @@
-# Poll project 
-https://docs.djangoproject.com/en/3.0/intro/tutorial01/
-
-Two parts:
-A public site that lets people view polls and vote in them.
-An admin site that lets you add, change, and delete poll
--- Projects Python/poll/poll/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    # allows referencing other URLconfs from polls
-    path('polls/', include('polls.urls')),
-    
-    path('admin/', admin.site.urls),
-]
